97493cb6f7c8bbd6d640eb011d1be6b7d5f0618f
sangeethkc <keyceesangi@gmail.com>
2024-08-31 05:36:40 +0530

    first commit with hello world

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    private static void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+}
